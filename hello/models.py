518161c156d80ce2c80b7cd507ec5a80d5324b04
Viljami <viljami.virolainen@aalto.fi>
2018-01-09 23:44:53 +0200

    added Game class and plans for other models as comments

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Game(models.Model):
+    name = models.
+    url = models.URLField()
+    name = models.CharField()
+    developer = models.CharField()
+    price = models.PositiveIntegerField()
+    image = models.ImageField()
+    description = models.TextField()
+
+#* Developer: name, email, password, games
+#* Player: name, email, password, games 
+
+#* Highscore: gameName, playerName, score
