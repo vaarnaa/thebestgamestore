dc3887f1e74569e8ed46cf814b4a248c1e9319e6
Antero Vaarnamo <vaarnaa1@aalto.fi>
2018-01-10 14:44:01 +0200

    Renamed project to thebestgamestore

--- thebestgamestore/settings.py
+++ thebestgamestore/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'thebestgamestore.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'thebestgamestore.wsgi.application'
 
 
 # Database
