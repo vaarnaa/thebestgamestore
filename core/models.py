be99366ff8ae84979a29d663a97bfd4fafeb61b2
Valtteri Lipsanen <valtteri.lipsanen@aalto.fi>
2018-01-28 13:35:39 +0200

    Added new models

--- core/models.py
+++ core/models.py
@@ -13,7 +13,27 @@ class Game(models.Model):
     image = models.ImageField()
     description = models.TextField()
 
-#* Developer: name, email, password, games
 #* Player: name, email, password, games
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    games = models.ManyToManyField(Game)
+
+    def get_id(self):
+        return user.id
+
+    def username(self):
+        return user.username
+
+#* Developer: name, email, password, games
+class Developer(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    games = models.ManyToManyField(Game)
+
+    def get_id(self):
+        return user.id
+
+    def username(self):
+        return user.username
 
 #* Highscore: gameName, playerName, score
