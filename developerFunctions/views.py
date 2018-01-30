1725183626165d58a8ba7d8ef322681c0e8d1128
Valtteri Lipsanen <valtteri.lipsanen@aalto.fi>
2018-01-30 15:35:28 +0200

    Some views -drafts on player and developer

--- developerFunctions/views.py
+++ developerFunctions/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404
 
-# Create your views here.
+from core.models import Game, Player
+
+def add_game(request, d_id, game_id):
+    developer = get_object_or_404(Developer, get_id=d_id)
+    game = get_object_or_404(Game, id=game_id)
+
+    developer.games.add(game)
+
+
+    #return something
+
+def create_developer(request, user_id):
+    current_user = get_object_or_404(User, id=user_id)
+
+    d = Developer(user = current_user)
+
+
+
+
+    #return something
