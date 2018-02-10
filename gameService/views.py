9e7d041335058bb6283e01dc03609ea62b2fac63
Viljami <viljami.virolainen@aalto.fi>
2018-02-10 13:23:19 +0200

    started working on gameservice and added the link to url to detail_owned

--- gameService/views.py
+++ gameService/views.py
@@ -1,11 +1,68 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404
 
+from core.models import Game, Player
 # Create your views here.
-def productview(request, product_id):
-    """
-    Write your view implementations for exercise 4 here.
-    Remove the current return line below.
-    """
-    gameUrl = http://webcourse.cs.hut.fi / example_game.html
+def game(request, game_id):
+    
+    player = get_object_or_404(Player, get_id=request.user.id)
+    game = get_object_or_404(Game, id=game_id)
+    gameUrl = game.url
+
+    return render(request, 'gameService/gameService.html', {'gameUrl': gameUrl})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def savescore(request, game_id):
+    
+    player = get_object_or_404(Player, get_id=player_id)
+    game = get_object_or_404(Game, id=game_id)
+    gameUrl = game.url
+
+    return render(request, 'gameService/gameService.html', {'gameUrl': gameUrl})
+
+def savegame(request, game_id):
+    
+    player = get_object_or_404(Player, get_id=player_id)
+    game = get_object_or_404(Game, id=game_id)
+    gameUrl = game.url
+
+    return render(request, 'gameService/gameService.html', {'gameUrl': gameUrl})
+
+def loadgame(request, game_id):
+    
+    player = get_object_or_404(Player, get_id=player_id)
+    game = get_object_or_404(Game, id=game_id)
+    gameUrl = game.url
+
     return render(request, 'gameService/gameService.html', {'gameUrl': gameUrl})
 
